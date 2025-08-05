7253a70c16bdc183c4fe8924b0d22406f62a6880
Rida-Lad <ridlad7@gmail.com>
2025-08-05 02:32:35 +0100

    First Implementation of the Backend of the project Planning-Travel

---
+++ nohighwaycities.py
@@ -0,0 +1,13 @@
+no_highway_pairs = {
+    ("Essaouira", "Marrakech"),
+    ("Essaouira", "Agadir"),
+    ("Fes", "Tetouan"),
+    ("Fes", "Dakhla"),
+    ("Agadir", "Dakhla"),
+    ("Essaouira", "Casablanca"),
+    ("Essaouira", "Rabat"),
+    ("Dakhla", "Tetouan"),
+    ("Dakhla", "Tanger"),
+    ("Dakhla", "Casablanca"),
+    ("Dakhla", "Rabat"),
+}
